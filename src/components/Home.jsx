955a4c104c5418969684ed0e79d1dde72db7f616
Emmanuel Ayodeji <hahyordeji@gmail.com>
2023-10-03 00:14:42 +0100

    changed home.jsx folder

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,5 @@
 import {useRef, useEffect} from 'react'
-import { useAssistant } from './components/AssistantContext';
+import { useAssistant } from './AssistantContext';
 
 function Home() {
 const {clearAssistant, triggerPopup} = useAssistant()
